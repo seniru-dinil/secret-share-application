2fccd7eddac2f6d5d83a61b30fe097c6751ec5b0
seniru-dinil <senirudinil927@gmail.com>
2025-05-27 19:09:45 +0530

    feat: add Dockerfile, docker-compose.yml, and initial SQL setup for database schema

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,19 @@
+import { configDotenv } from "dotenv";
 import mysql from "mysql2/promise";
-
+configDotenv();
 export let connection: any;
+
+const HOST = process.env.DB_HOST;
+const USER = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 (async () => {
   connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "secret_db",
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DB_NAME,
   });
 
   connection.connect((err: any) => {
